433b5ade52ba507ef523ea2bd32fee5aa0c5e610
mosesren <mosesren1021@gmail.com>
2024-10-30 00:46:45 +0800

    修改脚本

--- hostUpdate.sh
+++ hostUpdate.sh
@@ -4,7 +4,7 @@ begin=$(grep -n "# fetch-github-hosts begin" /etc/hosts|awk -F: '{print $1}')
 end=$(grep -n "# fetch-github-hosts end" /etc/hosts|awk -F: '{print $1}')
 echo "[INFO] Removing old hosts. Start at line \"${begin}\", End at line \"${end}\""
 if [ ! -z "$end" ] || [ ! -z "$start" ]; then
-    $end = (($end +1))
+    let end=$end+1
     echo $rootPass |sudo -S sed -i '' "${begin},${end}d" /etc/hosts
 fi
 echo $rootPass |sudo -S zsh -c  "curl https://hosts.gitcdn.top/hosts.txt >> /etc/hosts"
